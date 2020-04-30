219127635e10d16e1dd290dc8331d66bc9ee1c38
EvanStJohn <46731712+EvanStJohn@users.noreply.github.com>
2020-04-29 22:00:14 -0700

    add new tests

--- venv/data.py
+++ venv/data.py
@@ -1,6 +1,17 @@
 import random
 from perceptron import Perceptron
 
+def average(num,trainingSet,trainingLabels,set,labels):
+    sum = 0
+    for i in range(num):
+        perceptron = Perceptron(len(set[0]))
+        sum += perceptron.test(trainingSet,trainingLabels,set,labels)
+    avg = round(sum / num, 2)
+    print("Training Size: ", len(trainingSet))
+    print("Testing Size: ", len(set))
+    print("accurracy: ", avg, " %\n")
+
+print("test 1: 3x3 grid")
 trainingInputs1 = []
 trainingInputs1.append([0,1,0,0,1,0,0,1,0]) #middle i
 trainingInputs1.append([0,1,0,0,1,0,0,1,1]) #middle l
@@ -10,21 +21,67 @@ data1.append([0,0,1,0,0,1,0,0,1]) #right i
 data1.append([1,0,0,1,0,0,1,1,1]) #full l
 data1.append([1,0,0,1,0,0,1,1,0]) #left l
 data1.append([1,0,0,1,0,0,1,0,0]) #left i
-label1 = [0,1,1,0]
-perceptron1 = Perceptron(9)
-print("test 1: 2 training inputs")
-perceptron1.test(trainingInputs1,trainingLabels1,data1,label1)
+data1.append([0,0,1,0,0,1,0,0,0]) #small right i, upper
+data1.append([0,0,0,0,0,1,0,0,1]) #small right i, lower
+data1.append([0,1,0,0,1,0,0,0,0]) #small mid i, upper
+data1.append([0,0,0,0,1,0,0,1,0]) #small mid i, lower
+data1.append([1,0,0,1,0,0,0,0,0]) #small left i, upper
+data1.append([0,0,0,1,0,0,1,0,0]) #small left i, lower
+data1.append([0,1,0,0,1,1,0,0,0]) #small mid l, upper
+data1.append([0,0,0,0,1,0,0,1,1]) #small mid l, lower
+data1.append([1,0,0,1,1,0,0,0,0]) #small left l, upper
+data1.append([0,0,0,1,0,0,1,1,0]) #small left l, lower
+data1.append([1,1,0,1,1,0,1,1,0]) #large i, left
+data1.append([0,1,1,0,1,1,0,1,1]) #large i, right
+data1.append([1,1,0,1,1,0,1,1,1]) #large l, small bottom
+data1.append([1,1,0,1,1,1,1,1,1]) #large l, filled mid
+label1 = [0,1,1,0,0,0,0,0,0,0,1,1,1,1,0,0,1,1]
+average(10,trainingInputs1,trainingLabels1,data1,label1)
 
+print("test 2: 3x3 grid")
 trainingInputs2 = []
 trainingInputs2.append([0,1,0,0,1,0,0,1,0]) #middle i
 trainingInputs2.append([0,1,0,0,1,0,0,1,1]) #middle l
 trainingInputs2.append([1,0,0,1,0,0,1,1,0]) #left l
 trainingInputs2.append([1,0,0,1,0,0,1,0,0]) #left i
-trainingLabels2 = [0,1,1,0]
+trainingInputs2.append([0,0,1,0,0,1,0,0,0]) #small right i, upper
+trainingInputs2.append([1,0,0,1,1,0,0,0,0]) #small left l, upper
+trainingLabels2 = [0,1,1,0,0,1]
 data2 = []
 data2.append([0,0,1,0,0,1,0,0,1]) #right i
 data2.append([1,0,0,1,0,0,1,1,1]) #full l
-label2 = [0,1]
-perceptron2 = Perceptron(9)
-print("test 2: 4 training inputs")
-perceptron2.test(trainingInputs2,trainingLabels2,data2,label2)
+data2.append([0,0,0,0,0,1,0,0,1]) #small right i, lower
+data2.append([0,1,0,0,1,0,0,0,0]) #small mid i, upper
+data2.append([0,0,0,0,1,0,0,1,0]) #small mid i, lower
+data2.append([1,0,0,1,0,0,0,0,0]) #small left i, upper
+data2.append([0,0,0,1,0,0,1,0,0]) #small left i, lower
+data2.append([0,1,0,0,1,1,0,0,0]) #small mid l, upper
+data2.append([0,0,0,0,1,0,0,1,1]) #small mid l, lower
+data2.append([0,0,0,1,0,0,1,1,0]) #small left l, lower
+data2.append([1,1,0,1,1,0,1,1,0]) #large i, left
+data2.append([0,1,1,0,1,1,0,1,1]) #large i, right
+data2.append([1,1,0,1,1,0,1,1,1]) #large l, small bottom
+data2.append([1,1,0,1,1,1,1,1,1]) #large l, filled mid
+label2 = [0,1,0,0,0,0,0,1,1,1,0,0,1,1]
+average(10,trainingInputs2,trainingLabels2,data2,label2)
+
+print("test 3: 5x5 grid")
+trainingInputs3 = []
+trainingInputs3.append([0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0]) #standard middle i
+trainingInputs3.append([0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,1,1]) #standard middle l
+trainingLabels3 = [0,1]
+data3 = []
+data3.append([1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0]) #standard left i
+data3.append([1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,1,1,0,0]) #standard left l
+data3.append([0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0]) #standard mid-left i
+data3.append([0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,1,1,0]) #standard mid-left l
+data3.append([0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0]) #standard mid-right i
+data3.append([0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1]) #standard right i
+data3.append([1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,1,1,1,1]) #full-bottom left l
+data3.append([0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,1,1,1]) #full-bottom mid-left l
+data3.append([0,0,1,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,1,1]) #2-wide middle l
+data3.append([0,0,1,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,1,0]) #2-wide middle i
+data3.append([0,1,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,1,0,0]) #2-wide mid-left i
+data3.append([1,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,1,0,0,0]) #2-wide left i
+label3 = [0,1,0,1,0,0,1,1,1,0,0,0]
+average(10,trainingInputs3,trainingLabels3,data3,label3)
--- venv/perceptron.py
+++ venv/perceptron.py
@@ -2,14 +2,14 @@ import random
 
 class Perceptron(object):
 
-    def __init__(self, inputSize, bias=-1, epochs=100, learningRate=0.1):
+    def __init__(self, inputSize, bias=-1, epochs=100, learningRate=0.01):
         self.epochs = epochs
         self.learningRate = learningRate
         self.bias = bias
         self.weights = [0] * (inputSize + 1)
 
         for i in range(len(self.weights)):
-            self.weights[i] = round(random.uniform(-0.5,0.5), 2)
+            self.weights[i] = random.uniform(-0.5,0.5)
 
     def predict(self, input):
         summation = self.bias * self.weights[0]
@@ -22,12 +22,15 @@ class Perceptron(object):
 
     def train(self, trainingSet, labels):
         for epoch in range(self.epochs):
+            errors = 0
             for i in range(len(trainingSet)):
                 prediction = self.predict(trainingSet[i])
+                errors += (prediction - labels[i]) ** 2
                 input = trainingSet[i]
-                self.weights[0] = round(self.weights[0] - self.learningRate * (prediction - labels[i]) * self.bias, 2)
+                self.weights[0] = self.weights[0] - self.learningRate * (prediction - labels[i]) * self.bias
                 for j in range(len(input)):
-                    self.weights[j+1] = round(self.weights[j+1] - self.learningRate * (prediction - labels[i]) * input[j],2)
+                    self.weights[j+1] = self.weights[j+1] - self.learningRate * (prediction - labels[i]) * input[j]
+            #print("epoch",epoch, errors)
 
     def test(self,trainingSet,trainingLabels,set,labels):
         self.train(trainingSet,trainingLabels)
@@ -35,5 +38,4 @@ class Perceptron(object):
         for i in range(len(set)):
             if self.predict(set[i]) == labels[i]:
                 correct += 1
-        print("accuracy: ", (correct / len(set)) * 100, "%")
-        print("Final Weights: ", self.weights, "\n")
+        return (correct / len(set)) * 100
